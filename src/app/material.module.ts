fae9511cac29934e16a7b1cb342363429cd8c6c3
mertkanyener <mertkanyener@outlook.com>
2019-03-10 12:13:31 +0300

    Working on home screen

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 import {
   MatButtonModule,
-  MatCardModule, MatDialogModule, MatIconModule
+  MatCardModule, MatDialogModule, MatIconModule, MatToolbarModule
 } from '@angular/material';
 
 
@@ -11,13 +11,15 @@ import {
     MatButtonModule,
     MatCardModule,
     MatDialogModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule
   ],
   exports: [
     MatButtonModule,
     MatCardModule,
     MatDialogModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule {
