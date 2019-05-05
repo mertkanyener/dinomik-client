35f03a7d6a1264ce0d6b2985fdfb798bd8cce2bc
mertkanyener <mertkanyener@outlook.com>
2019-05-05 23:35:33 +0300

    05.05.2019

--- src/app/artist/artist-list/artist-list.component.ts
+++ src/app/artist/artist-list/artist-list.component.ts
@@ -9,19 +9,26 @@ import {Artist} from '../../shared/artist.model';
 })
 export class ArtistListComponent implements OnInit {
 
-  artists : Artist[];
+  artists: Artist[];
+  artistCount: number;
+  rowNum: number;
 
   constructor(private artistService: ArtistService) { }
 
   ngOnInit() {
-
     this.artists = this.artistService.getArtists();
-    console.log("Artists: ", this.artists);
+    console.log('Artists: ', this.artists);
     this.artistService.artistsChanged.subscribe(
       (artists: Artist[]) => {
         this.artists = artists;
+        this.artistCount = this.artists.length;
+        this.rowNum = this.artistCount / 5;
       }
     );
+  }
+
+  createArray(n: number): any[] {
+    return Array(n);
   }
 
 
