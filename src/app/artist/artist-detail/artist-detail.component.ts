92227eff139817dcba5e14a130ddfb51aca7d339
mertkanyener <mertkanyener@outlook.com>
2019-05-22 20:04:17 +0300

    ASASDAS

--- src/app/artist/artist-detail/artist-detail.component.ts
+++ src/app/artist/artist-detail/artist-detail.component.ts
@@ -1,3 +1,4 @@
+import { HttpService } from 'src/app/shared/http.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Artist } from './../../shared/artist.model';
@@ -19,13 +20,15 @@ export class ArtistDetailComponent implements OnInit {
 
   constructor(private artistService: ArtistService,
               private route: ActivatedRoute,
-              private sanitizer: DomSanitizer) { }
+              private sanitizer: DomSanitizer,
+              private httpService: HttpService) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
-        this.artist = this.artistService.getArtist(this.id);
+        this.artist = this.httpService.getArtist(this.id);
+        // this.artist = this.artistService.getArtist(this.id);
         this.avatarStyle = {
           'background-image': this.sanitizer.bypassSecurityTrustUrl(this.artist.image),
           'background-size': 'cover'
--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -18,100 +18,113 @@ export class HttpService {
               private artistService: ArtistService,
               private eventService: EventService,
               private venueService: VenueService,
-              private authService: AuthService){
+              private authService: AuthService) {
 
   }
 
 
   // Artist Operations
 
-  getArtists(){
+  getArtist(id: number): Artist {
+    let result = new Artist();
+    this.http.get<Artist>(this.path + 'artists/' + id).subscribe(
+      (artist: Artist) => {
+        result = artist;
+      },
+      (error) => {
+        console.log('ERROR: ', error);
+      }
+    );
+    return result;
+  }
+
+  getArtists() {
     this.http.get<Artist[]>(this.path + 'artists').subscribe(
       (artists) => {
         this.artistService.setArtists(artists);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
-  deleteArtist(id: number){
+  deleteArtist(id: number) {
     this.http.delete(this.path + 'admin/artists/' + id, this.authService.httpOptions).subscribe(
       (res) => {
         this.artistService.deleteArtist(id);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
-  updateArtist(id: number, artist: Artist){
+  updateArtist(id: number, artist: Artist) {
     this.http.put(this.path + 'admin/artists/' + id, artist, this.authService.httpOptions).subscribe(
       (res) => {
         this.artistService.updateArtist(id, artist);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
-  addArtist(artist: Artist){
+  addArtist(artist: Artist) {
     this.http.post(this.path + 'admin/artists', artist, this.authService.httpOptions).subscribe(
       (res) => {
         this.artistService.addArtist(artist);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
   // Event Operations
 
-  getEvents(){
+  getEvents() {
     this.http.get<Event[]>(this.path + 'events').subscribe(
       (events) => {
         this.eventService.setEvents(events);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
-  deleteEvent(id: number){
+  deleteEvent(id: number) {
 
     this.http.delete(this.path + 'admin/events/' + id, this.authService.httpOptions).subscribe(
       (res) => {
         this.eventService.deleteEvent(id);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
-  updateEvent(id: number, event: Event){
+  updateEvent(id: number, event: Event) {
     this.http.put(this.path + 'admin/events/' + id, event, this.authService.httpOptions).subscribe(
       (res) => {
         this.eventService.updateEvent(id, event);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
-  addEvent(event: Event){
+  addEvent(event: Event) {
     this.http.post(this.path + 'admin/events/', event, this.authService.httpOptions).subscribe(
       (res) => {
         this.eventService.addEvent(event);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
@@ -119,65 +132,65 @@ export class HttpService {
 
   // Venue Operations
 
-  getVenues(){
+  getVenues() {
     this.http.get<Venue[]>(this.path + 'venues').subscribe(
       (venues) => {
         this.venueService.setVenues(venues);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
-  deleteVenue(id: number){
+  deleteVenue(id: number) {
     this.http.delete(this.path + 'admin/venues/' + id, this.authService.httpOptions).subscribe(
       (res) => {
         this.venueService.deleteVenue(id);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
-  updateVenue(id: number, venue: Venue){
+  updateVenue(id: number, venue: Venue) {
     this.http.put(this.path + 'admin/venues/' + id, venue, this.authService.httpOptions).subscribe(
       (res) => {
         this.venueService.updateVenue(id, venue);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
   }
 
-  addVenue(venue: Venue){
+  addVenue(venue: Venue) {
     this.http.post(this.path + 'admin/venues', venue, this.authService.httpOptions).subscribe(
       (res) => {
         this.venueService.addVenue(venue);
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
-    )
+    );
   }
 
   uploadImage(image: File, type: string, name: string): number {
 
     let status: number;
-    const req = new HttpRequest("POST", this.path + 'admin/images/' + type, image, this.authService.httpOptions )
+    const req = new HttpRequest('POST', this.path + 'admin/images/' + type, image, this.authService.httpOptions );
     this.http.request(req).pipe(map(
-      (res : HttpResponse<any>) => {
+      (res: HttpResponse<any>) => {
         status = res.status;
-        console.log("Status: ", status);
+        console.log('Status: ', status);
       }
    )).subscribe(
       (resp) => {
-        console.log("Image uploaded successfully!");
+        console.log('Image uploaded successfully!');
       },
       (error) => {
-        console.log("ERROR: ", error);
+        console.log('ERROR: ', error);
       }
     );
 
