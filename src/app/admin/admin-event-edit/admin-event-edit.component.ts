a0e011672e3713328025da4f759605ce236690fb
mertkanyener <mertkanyener@outlook.com>
2019-11-18 23:08:15 +0300

    18.11.2019 - Monday

--- src/app/admin/admin-event-edit/admin-event-edit.component.ts
+++ src/app/admin/admin-event-edit/admin-event-edit.component.ts
@@ -1,6 +1,12 @@
+import { VenueHttpService } from './../../venue/venue-http.service';
+import { VenueService } from 'src/app/venue/venue.service';
+import { ArtistService } from 'src/app/artist/artist.service';
+import { ArtistHttpService } from 'src/app/artist/artist-http.service';
+import { Venue } from 'src/app/shared/venue.model';
+import { Artist } from './../../shared/artist.model';
 import { DomSanitizer } from '@angular/platform-browser';
 import { UtilityService } from 'src/app/shared/utility.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { EventHttpService } from 'src/app/event/event-http.service';
 import { EventService } from 'src/app/event/event.service';
 import { Subscription } from 'rxjs';
@@ -19,23 +25,52 @@ export class AdminEventEditComponent implements OnInit, OnDestroy {
   editMode = false;
   id: number;
   event = new Event();
+  artists: Artist[];
+  allArtists: Artist[];
+  venue: Venue;
+  allVenues: Venue[];
   form: FormGroup;
   image = new Image('', null);
   imageSubscription: Subscription;
   eventSubscription: Subscription;
+  artistSubscription: Subscription;
+  venueSubscription: Subscription;
 
   constructor(public eventService: EventService,
               public eventHttpService: EventHttpService,
+              public artistHttpService: ArtistHttpService,
+              public artistService: ArtistService,
+              public venueService: VenueService,
+              public venueHttpService: VenueHttpService,
               public route: ActivatedRoute,
               public router: Router,
               public utilService: UtilityService,
               public sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        if (params['id'] != null) {
+          this.editMode = true;
+          if (this.eventService.getEvent(this.id) === undefined) {
+            this.eventHttpService.getEvent(this.id);
+            this.eventSubscription = this.eventService.eventChanged.subscribe(
+              (event: Event) => {
+                this.event = event;
+                
+              }
+            );
+          }
+        }
+      }
+    );
   }
 
   private initForm() {
     let name = '';
+
+    
     
   }
 
